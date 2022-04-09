d04e2fde3406747fa005acac7499b3a5659c6224
Hendi Santika <hendisantika@gmail.com>
2022-04-09 14:53:01 +0700

    #15 - Add createEmployee in EmployeeController

--- src/main/java/com/hendisantika/controller/EmployeeController.java
+++ src/main/java/com/hendisantika/controller/EmployeeController.java
@@ -1,7 +1,12 @@
 package com.hendisantika.controller;
 
+import com.hendisantika.model.Employee;
 import com.hendisantika.service.EmployeeService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
@@ -19,4 +24,8 @@ public class EmployeeController {
     @Autowired
     private EmployeeService employeeService;
 
+    @PostMapping(consumes = "application/json", produces = "application/json", path = "/employee")
+    public ResponseEntity<Employee> createEmployee(@RequestBody Employee emp) {
+        return new ResponseEntity<>(employeeService.createEmployee(emp), HttpStatus.CREATED);
+    }
 }
